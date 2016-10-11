e24d35bb7b9e89ccc716b29e6c02901c5caa09fc
XiaoKe <fighttime@sina.cn>
2016-10-11 10:54:28 +0800

    Initial Commit

---
+++ FuLingOnline-CheerforAndLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FuLingOnline-CheerforAndLearning
+//
+//  Created by 肖科 on 16/10/11.
+//  Copyright © 2016年 com.FuLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FuLingOnline-CheerforAndLearning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FuLingOnline-CheerforAndLearning
+//
+//  Created by 肖科 on 16/10/11.
+//  Copyright © 2016年 com.FuLing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
