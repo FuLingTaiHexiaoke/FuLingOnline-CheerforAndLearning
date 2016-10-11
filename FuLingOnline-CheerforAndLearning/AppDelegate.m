0e72726f2bfa9222d8c72e883a90d2027f47d56d
XiaoKe <fighttime@sina.cn>
2016-10-11 16:25:33 +0800

    no time
    no time

--- FuLingOnline-CheerforAndLearning/AppDelegate.m
+++ FuLingOnline-CheerforAndLearning/AppDelegate.m
@@ -14,9 +14,25 @@
 
 @implementation AppDelegate
 
+#pragma mark -UIApplication Lifecircle
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //输出路径
+#if DEBUG
+    NSLog(@"\n\nSimulator documents directory:\n\t%@\n\n",
+          [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]);
+#endif
+    //注册消息推送
+    [self registerRemoteNotification:application];
+    [self registerLocalNotification:5];
+    
+    
+    //收集AppLaunchDate，AppInstallDateIfNil，logAppNumberOfDaysSinceInstall
+    //初始化RootView（广告页面等）
+    //初始化基本控件Appearance
+    //
+    //
+    //
     return YES;
 }
 
@@ -42,4 +58,116 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+#pragma mark - Private Methods
+
+ //输出路径
+-(void)logAppLoactionPath{
+#if DEBUG
+    NSLog(@"\n\nSimulator documents directory:\n\t%@\n\n",
+          [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]);
+#endif
+}
+
+//注册消息推送
+-(void)registerRemoteNotification:(UIApplication *)application{
+    //http://blog.csdn.net/zhuming3834/article/details/49001809 细致
+    //http://blog.csdn.net/apple_app/article/details/39228221  全面
+    //http://blog.csdn.net/yujianxiang666/article/details/35260135 细致
+    //注册消息推送
+    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        // for iOS 8
+        UIUserNotificationSettings* settings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [application registerForRemoteNotifications];
+    }
+    //不再考虑  iOS 7 or iOS 6
+    //    else {
+    //        // for iOS 7 or iOS 6
+    //        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound | UIRemoteNotificationTypeAlert)];
+    //    }
+}
+
+
+/**
+ *  注册一个本地推送
+ *
+ *  @param alertTime 推送间隔时间
+ */
+- (void)registerLocalNotification:(NSInteger)alertTime {
+    NSLog(@"发送通知");
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    // 设置触发通知的时间
+    NSDate *fireDate = [NSDate dateWithTimeIntervalSinceNow:alertTime];
+    notification.fireDate = fireDate;
+    // 时区  时区跟随时区变化
+    notification.timeZone = [NSTimeZone defaultTimeZone];
+    // 设置重复的间隔
+    notification.repeatInterval = kCFCalendarUnitSecond;
+    // 通知内容
+    notification.alertBody =  @"iOS本地推送";
+    // 设置icon右上角的显示数
+    notification.applicationIconBadgeNumber = 1;
+    // 通知被触发时播放的声音
+    notification.soundName = UILocalNotificationDefaultSoundName;
+    // 通知参数
+    NSDictionary *userDict = [NSDictionary dictionaryWithObject:@"iOS本地推送Demo" forKey:@"key"];
+    notification.userInfo = userDict;
+    
+    // ios8后，需要添加这个注册，才能得到授权
+    if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+        UIUserNotificationType type =  UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:type
+                                                                                 categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        // 通知多长时间重复提示
+        notification.repeatInterval = NSCalendarUnitSecond;
+    }
+    else {
+        // 通知多长时间重复提示
+        notification.repeatInterval = NSCalendarUnitSecond;
+    }
+    // 执行注册的通知
+    [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+}
+
+#pragma mark - RemoteNotification CallBack
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    NSString *token = [NSString stringWithFormat:@"%@", deviceToken];
+    NSString *dt = [token stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    NSString *dn = [dt stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSLog(@"获取令牌:  %@",dn);
+    //    TheRuntime.pushToken= [dn stringByReplacingOccurrencesOfString:@" " withString:@""];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSString *error_str = [NSString stringWithFormat: @"%@", error];
+    NSLog(@"获取令牌失败:  %@",error_str);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo{
+    
+}
+
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification{
+    NSLog(@"notification:%@",notification);
+    NSString *notMess = [notification.userInfo objectForKey:@"key"];
+    UIAlertController* alertController=[UIAlertController alertControllerWithTitle:@"正在查看本地通知" message:notMess preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction*  alertAction=[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"%@",notMess);
+    }];
+    [alertController addAction:alertAction];
+    
+//    UIAlertController *alert = [[UIAlertView alloc] initWithTitle:@"正在查看本地通知"
+//                                                    message:notMess
+//                                                   delegate:nil
+//                                          cancelButtonTitle:@"确定"
+//                                          otherButtonTitles:nil];
+//    [alert show];
+    // 更新applicationIconBadgeNumber
+    NSInteger badge = [UIApplication sharedApplication].applicationIconBadgeNumber;
+    badge--;
+    badge = badge >= 0 ? badge : 0;
+    [UIApplication sharedApplication].applicationIconBadgeNumber = badge;
+}
 @end
