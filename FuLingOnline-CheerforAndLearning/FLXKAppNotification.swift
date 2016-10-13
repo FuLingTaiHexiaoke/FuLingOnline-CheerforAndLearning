54e6cb273796e2cb43defa3794ca410df9e9d5aa
XiaoKe <fighttime@sina.cn>
2016-10-13 15:59:24 +0800

    目前在swift3.0统一之前去掉swift
    目前在swift3.0统一之前去掉swift

--- FuLingOnline-CheerforAndLearning/FLXKAppNotification.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FLXKAppNotification.swift
-//  FuLingOnline-CheerforAndLearning
-//
-//  Created by 肖科 on 16/10/12.
-//  Copyright © 2016年 com.FuLing. All rights reserved.
-//
-
-import Foundation
