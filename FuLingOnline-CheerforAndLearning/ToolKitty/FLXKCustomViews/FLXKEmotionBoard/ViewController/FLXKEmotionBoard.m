7a17e83fb0334a6f6da14728742718c186695f3e
XiaoKe <fighttime@sina.cn>
2017-02-10 18:29:05 +0800

    pagecontrol还没有弄好

--- FuLingOnline-CheerforAndLearning/ToolKitty/FLXKCustomViews/FLXKEmotionBoard/ViewController/FLXKEmotionBoard.m
+++ FuLingOnline-CheerforAndLearning/ToolKitty/FLXKCustomViews/FLXKEmotionBoard/ViewController/FLXKEmotionBoard.m
@@ -20,6 +20,7 @@
 #import "NSAttributedString+EmotionExtension.h"
 #import "UIImage+GIF.h"
 #import "FLXKEmotionConfig.h"
+#import "KYAnimatedPageControl.h"
 //entity
 #import "EmotionGroup.h"
 #import "EmotionItem.h"
@@ -37,7 +38,8 @@
 @property(nonatomic,strong)UIView *swithButton;
 
 //IBOutlet views
-@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (weak, nonatomic) IBOutlet UIView *pageControlPlaceholder;
+@property (strong, nonatomic) KYAnimatedPageControl *pageControl;
 @property (weak, nonatomic) IBOutlet FLXKEmotionShowingScrollView *emotionContainerScrollView;
 @property (weak, nonatomic) IBOutlet FLXKEmotionGroupIndexCollectionView *emotionGroupIndexCollectionView;
 @property (weak, nonatomic) IBOutlet UIButton *leftBottomButton;
@@ -99,6 +101,7 @@
     //        [self setDelegateForSubViewsInScrollView];
     _emotionGroupIndexCollectionView.emotionGroupSelectedDelegate=self;
     
+    [self setupUIPageControl];
 }
 
 //-(void)setDelegateForSubViewsInScrollView{
@@ -260,30 +263,101 @@
 }
 
 
-#pragma mark - UIScrollViewDelegate
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+-(void)setupUIPageControl{
+    //    //setup the pagecontroller
+    //    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, self.frame.size.height-20, self.frame.size.width,20)];
+    //    //    pageControl.numberOfPages = 6; // 一共显示多少个圆点（多少页）
+    //    // 设置非选中页的圆点颜色
+    //    pageControl.pageIndicatorTintColor = [UIColor grayColor];
+    //    // 设置选中页的圆点颜色
+    //    pageControl.currentPageIndicatorTintColor = [UIColor blueColor];
+    //    // 禁止默认的点击功能
+    //    pageControl.enabled = YES;
+    //    [self addSubview:pageControl];
+    //    _pageControl = pageControl;
+//    self.pageControlPlaceholder.superview.frame
+    NSLog(@"self.pageControlPlaceholder.superview.frame:%@", NSStringFromCGRect(self.pageControlPlaceholder.superview.frame));
     
-}
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
-    
-}
-- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
-    
-}
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
-    
-}- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
-    
-}
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
-    [_emotionContainerScrollView setCurrentShowingPageIndex:(NSInteger) scrollView.contentOffset.x/(Screen_Width)] ;
+ CGRect frame=  [self.pageControlPlaceholder convertRect:self.pageControlPlaceholder.frame toView:self];
+//        NSLog(@"frame1:%@", NSStringFromCGRect(frame));
+//    
+//   frame=  [self convertRect:self.pageControlPlaceholder.superview.frame toView:self];
+//    NSLog(@"frame2:%@", NSStringFromCGRect(frame));
+//
+//    frame=  [self.pageControlPlaceholder convertRect:self.pageControlPlaceholder.frame toView:self];
+//    NSLog(@"frame3:%@", NSStringFromCGRect(frame));
+//    
+//    frame=  [self.pageControlPlaceholder.superview convertRect:self.pageControlPlaceholder.frame toView:self];
+//    NSLog(@"frame4:%@", NSStringFromCGRect(frame));
+//    
+    self.pageControl = [[KYAnimatedPageControl alloc]
+                        initWithFrame:self.pageControlPlaceholder.superview.frame];
+    self.pageControl.pageCount = 8;
+    self.pageControl.unSelectedColor = [UIColor colorWithWhite:0.9 alpha:1];
+    self.pageControl.selectedColor = [UIColor redColor];
+    self.pageControl.bindScrollView = self.emotionContainerScrollView;
+    self.pageControl.shouldShowProgressLine = NO;
+    self.pageControl.indicatorStyle = IndicatorStyleGooeyCircle;
+    self.pageControl.indicatorSize = 20;
+    self.pageControl.swipeEnable = YES;
+     [self addSubview:self.pageControl];
+    self.pageControl.didSelectIndexBlock = ^(NSInteger index) {
+        NSLog(@"Did Selected index : %ld", (long)index);
+    };
 }
 
-- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
+//
+//#pragma mark - UIScrollViewDelegate
+//
+//- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+//    
+//}
+//- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView{
+//    
+//}
+//- (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
+//    
+//}
+//- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
+//    
+//}- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView{
+//    
+//}
+//- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+//
+//}
+//
+//- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView{
+//    
+//}
+
+#pragma mark-- UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // Indicator动画
+    [self.pageControl.indicator animateIndicatorWithScrollView:scrollView
+                                                  andIndicator:self.pageControl];
     
+    if (scrollView.dragging || scrollView.isDecelerating || scrollView.tracking) {
+        //背景线条动画
+        [self.pageControl.pageControlLine
+         animateSelectedLineWithScrollView:scrollView];
+    }
 }
 
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    self.pageControl.indicator.lastContentOffset = scrollView.contentOffset.x;
+        [_emotionContainerScrollView setCurrentShowingPageIndex:(NSInteger) scrollView.contentOffset.x/(Screen_Width)] ;
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
+    [self.pageControl.indicator
+     restoreAnimation:@(1.0 / self.pageControl.pageCount)];
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+    self.pageControl.indicator.lastContentOffset = scrollView.contentOffset.x;
+}
 //初始化表情视图
 -(id)initWithFrame:(CGRect)frame editingTextView:(UITextView*)editingTextView containerView:(UIView*)containerView{
     
@@ -319,7 +393,7 @@
         [self addSubview:self.faceScroll];
         
         [self setupUIPageControl];
-        [self drawEmotionViews];
+//        [self drawEmotionViews];
         
         self.editingTextView=editingTextView;
         
@@ -382,74 +456,59 @@
 }
 
 
--(void)setupUIPageControl{
-    //setup the pagecontroller
-    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:CGRectMake(0, self.frame.size.height-20, self.frame.size.width,20)];
-    //    pageControl.numberOfPages = 6; // 一共显示多少个圆点（多少页）
-    // 设置非选中页的圆点颜色
-    pageControl.pageIndicatorTintColor = [UIColor grayColor];
-    // 设置选中页的圆点颜色
-    pageControl.currentPageIndicatorTintColor = [UIColor blueColor];
-    // 禁止默认的点击功能
-    pageControl.enabled = YES;
-    [self addSubview:pageControl];
-    _pageControl = pageControl;
-    
-}
-
-//draw the emotion views and add to srollview
--(void)drawEmotionViews{
-    float width=  CGRectGetWidth(self.frame);
-    float height= CGRectGetHeight(self.frame);
-    float face_width=32;
-    float face_height=32;
-    //caculator out how many page shoulb be
-    int totalFaceCount= self.faces.count;
-    //how many face one row can contain
-    int eachRowFaceCount=(width-20)/face_width;
-    //how many  row can be
-    int totalRowCount=totalFaceCount/eachRowFaceCount+1;
-    //how many  rows can be in each view
-    int rowsInEachView=(height-20)/(face_height);
-    //
-    int pageCount=totalRowCount%rowsInEachView?totalRowCount/rowsInEachView+1:totalRowCount/rowsInEachView;
-    
-    int i=0;//rowIndex
-    int j=0;//faceIndex
-    //    //start to splite the faces into each view
-    for (int pageindex=0; pageindex<pageCount; pageindex++) {
-        UIView* page=[[UIView alloc]initWithFrame:CGRectMake(5+pageindex*(width),0, (width), height-30)];
-        for ( ; i<totalRowCount && i<(pageindex+1)*rowsInEachView; i++) {
-            for (int j=0; j<eachRowFaceCount && ((i*eachRowFaceCount)+j)<totalFaceCount; j++) {
-                //
-                UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-                button.frame = CGRectMake(10 + j*32, 10 + (i%rowsInEachView)*32, 32.0f, 32.0f);
-                NSMutableDictionary *tempdic = [self.faces objectAtIndex:((i*eachRowFaceCount)+j)];
-                NSString *key=[[tempdic allKeys] objectAtIndex:0];
-                
-                UIImage *tempImage = [tempdic valueForKey:key];
-                [button setBackgroundImage:tempImage forState:UIControlStateNormal];
-                
-                button.tag = ((i*eachRowFaceCount)+j);
-                
-                [button addTarget:self action:@selector(didSelectAFace:)forControlEvents:UIControlEventTouchUpInside];
-                
-                [page addSubview:button];
-                
-            }//for (int j=0; j<eachRowFaceCount;
-            
-            
-        }//for (int i=0; i<rowsInEachView; i++)
-        
-        [self.faceScroll addSubview:page];
-        
-    }// for (int pageindex=0; pageindex<pageCount; pageCount++)
-    
-    
-    self.pageControl.numberOfPages=pageCount;
-    [self.faceScroll setContentSize:CGSizeMake(pageCount*width, height)];
-    
-}
+////draw the emotion views and add to srollview
+//-(void)drawEmotionViews{
+//    float width=  CGRectGetWidth(self.frame);
+//    float height= CGRectGetHeight(self.frame);
+//    float face_width=32;
+//    float face_height=32;
+//    //caculator out how many page shoulb be
+//    int totalFaceCount= self.faces.count;
+//    //how many face one row can contain
+//    int eachRowFaceCount=(width-20)/face_width;
+//    //how many  row can be
+//    int totalRowCount=totalFaceCount/eachRowFaceCount+1;
+//    //how many  rows can be in each view
+//    int rowsInEachView=(height-20)/(face_height);
+//    //
+//    int pageCount=totalRowCount%rowsInEachView?totalRowCount/rowsInEachView+1:totalRowCount/rowsInEachView;
+//    
+//    int i=0;//rowIndex
+//    int j=0;//faceIndex
+//    //    //start to splite the faces into each view
+//    for (int pageindex=0; pageindex<pageCount; pageindex++) {
+//        UIView* page=[[UIView alloc]initWithFrame:CGRectMake(5+pageindex*(width),0, (width), height-30)];
+//        for ( ; i<totalRowCount && i<(pageindex+1)*rowsInEachView; i++) {
+//            for (int j=0; j<eachRowFaceCount && ((i*eachRowFaceCount)+j)<totalFaceCount; j++) {
+//                //
+//                UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+//                button.frame = CGRectMake(10 + j*32, 10 + (i%rowsInEachView)*32, 32.0f, 32.0f);
+//                NSMutableDictionary *tempdic = [self.faces objectAtIndex:((i*eachRowFaceCount)+j)];
+//                NSString *key=[[tempdic allKeys] objectAtIndex:0];
+//                
+//                UIImage *tempImage = [tempdic valueForKey:key];
+//                [button setBackgroundImage:tempImage forState:UIControlStateNormal];
+//                
+//                button.tag = ((i*eachRowFaceCount)+j);
+//                
+//                [button addTarget:self action:@selector(didSelectAFace:)forControlEvents:UIControlEventTouchUpInside];
+//                
+//                [page addSubview:button];
+//                
+//            }//for (int j=0; j<eachRowFaceCount;
+//            
+//            
+//        }//for (int i=0; i<rowsInEachView; i++)
+//        
+//        [self.faceScroll addSubview:page];
+//        
+//    }// for (int pageindex=0; pageindex<pageCount; pageCount++)
+//    
+//    
+//    self.pageControl.numberOfPages=pageCount;
+//    [self.faceScroll setContentSize:CGSizeMake(pageCount*width, height)];
+//    
+//}
 
 #pragma mark scrollDelegate
 
