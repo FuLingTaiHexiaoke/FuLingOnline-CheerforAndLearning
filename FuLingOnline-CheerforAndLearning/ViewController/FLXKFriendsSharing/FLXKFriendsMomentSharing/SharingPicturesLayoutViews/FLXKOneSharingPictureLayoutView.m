afc257e3d3d264cecdc4443ab24a85c2e5747779
XiaoKe <fighttime@sina.cn>
2017-04-05 18:39:50 +0800

    朋友圈网络请求图片有点眉目了

--- FuLingOnline-CheerforAndLearning/ViewController/FLXKFriendsSharing/FLXKFriendsMomentSharing/SharingPicturesLayoutViews/FLXKOneSharingPictureLayoutView.m
+++ FuLingOnline-CheerforAndLearning/ViewController/FLXKFriendsSharing/FLXKFriendsMomentSharing/SharingPicturesLayoutViews/FLXKOneSharingPictureLayoutView.m
@@ -31,8 +31,7 @@
 
 -(void)setImageArray:(NSArray<FLXKSharingImagesModel *> *)imageArray{
     [imageArray enumerateObjectsUsingBlock:^(FLXKSharingImagesModel * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        _SharingImageViews[idx].image=[UIImage imageNamed:obj];
-        [( (UIImageView*)(_SharingImageViews[idx])) sd_setImageWithURL:NSURL_BaseURL(obj.actualPictureUrl) placeholderImage:[UIImage imageNamed:@"Spark"]];
+        [( (UIImageView*)(_SharingImageViews[idx])) sd_setImageWithURL:NSURL_BaseURL(obj.thumbnailPictureUrl) placeholderImage:[UIImage imageNamed:@"Spark"]];
     }];
 }
 
